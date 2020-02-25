38c42c7327ff6d276cd0416ef0ff2f3e78033ee6
Dmitry <just3f@yandex.ru>
2020-02-25 10:28:55 +0300

    123

--- TestProject.WebAPI/RequestCounterMiddleware.cs
+++ TestProject.WebAPI/RequestCounterMiddleware.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Net;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 
 namespace TestProject.WebAPI
@@ -16,9 +17,18 @@ namespace TestProject.WebAPI
 
         public async Task InvokeAsync(HttpContext context)
         {
-            _counter++;
-            context.Response.Headers.Add("requestCounter", _counter.ToString());
-            await _next.Invoke(context);
+            string secretKeyConst = "passwordKey123456789";
+            var responseHeaders = context.Response.Headers;
+            responseHeaders.TryGetValue("secretKey", out var passwordKey);
+            if (passwordKey.Count > 0 && passwordKey[0] != null && passwordKey[0] == secretKeyConst)
+            {
+                await _next.Invoke(context);
+
+            }
+            else
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.Forbidden;
+            }
         }
     }
 }
